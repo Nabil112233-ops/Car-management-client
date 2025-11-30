341de4fc82bb95cf0df8e427a2a9094821a78eb1
Nabil112233 <junayedalnurnabil2@gmail.com>
2025-12-01 02:03:50 +0700

    Cors blocked are setup

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = import.meta.env.PROD
-    ? 'https://car-management-server.up.railway.app' // deployed server
+    ? 'https://car-management-server-iurf.vercel.app' 
     : 'http://localhost:5000';
