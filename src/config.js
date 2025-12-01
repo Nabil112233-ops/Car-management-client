795b82be065ba35bd0fa43d22d6b5d1ee1f67540
Nabil112233 <junayedalnurnabil2@gmail.com>
2025-12-02 01:00:58 +0700

    API updated

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = import.meta.env.PROD
-    ? 'https://car-management-server-henna.vercel.app' 
+    ? 'https://car-management-server-a39z.onrender.com' 
     : 'http://localhost:5000';
